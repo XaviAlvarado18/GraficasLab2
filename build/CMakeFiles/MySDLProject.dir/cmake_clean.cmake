7f55b4d4fa5c5b1a842e5d8b83c97047678d065d
XaviAlvarado18 <alv21188@uvg.edu.gt>
2023-07-21 17:56:41 -0600

    Implementacion de ConwaysOfLife

---
+++ build/CMakeFiles/MySDLProject.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MySDLProject.dir/WinMain.cpp.obj"
+  "CMakeFiles/MySDLProject.dir/WinMain.cpp.obj.d"
+  "MySDLProject.exe"
+  "MySDLProject.exe.manifest"
+  "MySDLProject.pdb"
+  "libMySDLProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MySDLProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
